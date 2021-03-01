eb8e90b0738aef4070265dd452b6d956f4ce5cd6
ndoom90 <ndoom90@gmail.com>
2021-03-01 23:46:05 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,6 @@
 
 using namespace std;
 
-struct Node {
-    int data;
-    Node *next;
-};
-
-
 int main() {
 
     LinkedList list = LinkedList();
@@ -31,22 +25,6 @@ int main() {
     list.remove(2);
     cout << list.toString() << endl;
     cout << "Length: " << list.length() << endl;
-
-//    Node *head;
-//    head = (Node *)malloc(sizeof(Node));
-//
-//    Node *node1 = (Node *)malloc(sizeof(Node));
-//    Node *node1;
-//    node1 = (Node *)malloc(sizeof(Node));
-//    head->next = node1;
-//    node1->data = 1;
-//
-//    Node *node2;
-//    node2 = (Node *)malloc(sizeof(Node));
-//    node1->next = node2;
-//    node2->data = 2;
-//
-//    cout << head->next->data << endl << head->next->next->data;
 
     return 0;
 }
